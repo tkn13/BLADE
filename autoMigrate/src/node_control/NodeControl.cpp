06ee1d32359660475623711ea0117c0541098c27
tkn13 <thaksin.ckn@gmail.com>
2026-03-10 15:20:50 +0700

    add node_control check job state

--- autoMigrate/src/node_control/NodeControl.cpp
+++ autoMigrate/src/node_control/NodeControl.cpp
@@ -4,9 +4,14 @@
 int main(){
 
     std::vector<JobState> output = get_job_state();
+    std::vector<NodeState> nodeState = get_node_state();
 
         for(JobState j : output){
             std::cout << j.job_id << " " << j.job_state << std::endl;
+        }
+
+        for(NodeState n : nodeState){
+            std::cout << n.node_id << " " << n.node_state << std::endl;
         }
 
     return 0;
